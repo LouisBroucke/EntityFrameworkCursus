2d278ef097f0a460b12a20a1a24c50728d98c0f1
LouisBroucke <louis.broucke@outlook.com>
2020-06-24 13:39:45 +0200

    Add project files.

---
+++ Model/Entities/Campus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Entities
+{
+    public class Campus
+    {
+        //Constructor
+        public Campus()
+        {
+            Docenten = new List<Docent>();
+        }
+
+        //Properties
+        public int CampusId { get; set; }
+        public string Naam { get; set; }
+        public string Straat { get; set; }
+        public string Huisnummer { get; set; }
+        public string Postcode { get; set; }
+        public string Gemeente { get; set; }
+        public string Commentaar { get; set; }
+
+        //Navigation Properties
+        public virtual ICollection<Docent> Docenten { get; set; }
+    }
+}
---
+++ Model/Entities/Docent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Model.Entities
+{
+    public partial class Docent
+    {
+        //Properties
+        public int DocentId { get; set; }
+        public string Voornaam { get; set; }
+        public string Familienaam { get; set; }
+        public decimal Wedde { get; set; }
+        public DateTime InDienst { get; set; }
+        public bool? HeeftRijbewijs { get; set; }
+        public string LandCode { get; set; }
+        public int CampusId { get; set; }
+
+        //Navigation Properties
+        public virtual Campus Campus { get; set; }
+        public virtual Land Land { get; set; }
+    }
+}
